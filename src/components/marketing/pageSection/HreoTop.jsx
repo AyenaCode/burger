4995037c1ac3ad7f9563d31c4624c019be38759a
AyenaCode <ayenacode1@gmail.com>
2024-09-19 07:09:02 -0400

    hero top intro

--- src/components/marketing/pageSection/HreoTop.jsx
+++ src/components/marketing/pageSection/HreoTop.jsx
@@ -1,4 +1,5 @@
 import bg from "../../../assets/images/background/bg-food.jpg";
+import hero from "../../../assets/images/background/burger-hero-top.png";
 
 import { Container } from "../../elements/Container";
 import { Header } from "./Header";
@@ -12,6 +13,25 @@ export const HreoTop = () => {
       >
         <Container>
           <Header />
+          <div className="w-full flex items-center">
+            <div className="w-full text-mysecondary text-xl 2/3 uppercase">
+              <h1 className=" ">
+                C'est le moment de gouter au bon gout des hangburgesr
+              </h1>
+              <h2 className="font-secondary ">
+                <span className="text-8xl block ">Burger</span>
+                <span className="text-6xl"> house</span>
+                <span className="text-4xl ml-3">Click&collect</span>
+              </h2>
+            </div>
+            <div className="w-1/3">
+              <img
+                src={hero}
+                alt="un menu buregr avec frites et coca-cola"
+                className=""
+              />
+            </div>
+          </div>
         </Container>
       </div>
     </div>
